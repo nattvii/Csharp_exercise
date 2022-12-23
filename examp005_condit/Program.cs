fabb65fc684f4a650088ef04bf0cf64c054ce50b
nattvii <natt.mmd@gmail.com>
2022-12-23 22:33:54 +0300

    condition

---
+++ examp005_condit/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.WriteLine("Fill in your username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey! It`s Masha!");
+}
+else
+{
+    Console.WriteLine("Hi, ");
+    Console.WriteLine(username);
+}
+
