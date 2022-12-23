5b0452138d9df7ac9311695d952f355263032b4e
nattvii <natt.mmd@gmail.com>
2022-12-23 22:02:39 +0300

    random

--- examp003_Sum/Program.cs
+++ examp003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numnerB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numnerB = new Random().Next(1, 10);
+Console.WriteLine(numnerB);
 Console.WriteLine(numberA + numnerB);
